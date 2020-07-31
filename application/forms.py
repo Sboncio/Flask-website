c43c37cfab537cab04dd8177467291930912827b
sboncio <samuelasquith@btinternet.com>
2020-07-31 13:23:49 +0000

    Added route to view and add robot models

---
+++ application/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Length
+
+class RobotForm(FlaskForm):
+    model_name = StringField('Model Name',
+        validators= [
+            DataRequired(),
+            Length(min=2, max=50)
+        ]
+    )
+
+    drive_type = StringField('Drive Type',
+        validators=[
+            DataRequired(),
+            Length(min=2,max=50)
+        ]
+    )
+    
+    height = IntegerField('Height(mm)')
+    width = IntegerField('Width(mm)')
+    length = IntegerField('Length(mm)')
+
+    submit = SubmitField('Add!')
--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,36 @@
-from flask import render_template
-from application import app
+from flask import render_template, redirect, url_for
+from application import app, db
+from application.models import Robots
+from application.forms import RobotForm
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title='Home')
+    return render_template('home.html',title='Home')
+
+@app.route('/robots')
+def robot():
+    robotData = Robots.query.all()
+    return render_template('robot.html',title='Robot',robots=robotData)
+
+@app.route('/add_robot', methods=['GET','POST'])
+def addRobot():
+    form = RobotForm()
+    if form.validate_on_submit():
+        robotData = Robots(
+            model_name = form.model_name.data,
+            drive_type = form.drive_type.data,
+            height = form.height.data,
+            width = form.width.data,
+            length = form.length.data
+        )
+
+        db.session.add(robotData)
+        db.session.commit()
+
+        return redirect(url_for('robot'))
+
+    else:
+        print(form.errors)
+
+    return render_template('add_robot.html', title='Add Robot', form=form)
